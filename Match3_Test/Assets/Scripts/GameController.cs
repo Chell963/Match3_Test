d7c3a753467c4c8a7ebb0342b5f4534d19349c3e
Chell963 <buterbrod963@gmail.com>
2022-07-18 15:17:35 +1000

    Point OnClick animation

--- Match3_Test/Assets/Scripts/GameController.cs
+++ Match3_Test/Assets/Scripts/GameController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -13,17 +15,58 @@ public class GameController : MonoBehaviour
 
     [SerializeField] private Canvas mainCanvas;
 
+    [SerializeField] private Image inputBLocker;
+
+    private List<List<Point>> pointList = new List<List<Point>>();
+
+    private int clicksNumber;
+
     private void Start()
     {
+        inputBLocker.gameObject.SetActive(false);
         for (var i = 0; i < length; i++)
         {
+            pointList.Add(new List<Point>());
+            var newList = pointList[i];
             for (var j = 0; j < length; j++)
             {
                 var newPoint = Instantiate(basicPoint, pointParent.transform);
-                newPoint.Setup(j,length-1 - i);
+                newPoint.Setup(i,j);
                 var pixelRect = mainCanvas.pixelRect;
                 newPoint.transform.position = 
                     new Vector2(-350 + j*100 + pixelRect.width/2,350 - i*100 + pixelRect.height/2);
+                newPoint.Selected += ClicksCount;
+                newList.Add(newPoint);
+            }
+        }
+    }
+
+    private async void ClicksCount()
+    {
+        clicksNumber++;
+        if (clicksNumber > 1)
+        {
+            inputBLocker.gameObject.SetActive(true);
+            clicksNumber = 0;
+            foreach (var points in pointList)
+            {
+                foreach (var point in points)
+                {
+                    point.Deselect();
+                }
+            }
+            await Task.Delay(TimeSpan.FromSeconds(1f));
+            inputBLocker.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        foreach (var points in pointList)
+        {
+            foreach (var point in points)
+            {
+                point.Selected -= ClicksCount;
             }
         }
     }
--- Match3_Test/Assets/Scripts/Point.cs
+++ Match3_Test/Assets/Scripts/Point.cs
@@ -1,26 +1,41 @@
+using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public enum Shape
+{
+    Heart,
+    Square,
+    Romb,
+    Circle,
+    Rectangle
+}
 
 public class Point : MonoBehaviour
 {
-    public int x;
-    
-    public int y;
-    public enum Shape
-    {
-        Heart,
-        Square,
-        Romb,
-        Circle,
-        Rectangle
-    }
+    public event Action Selected;
+
+    [HideInInspector] public int x;
+    [HideInInspector] public int y;
 
     [SerializeField] private Image image;
 
     [SerializeField] private List<Sprite> iconList;
 
+    [SerializeField] private Button button;
+
     private Shape pointShape;
+
+    private bool isSelected;
+
+    private bool isScaled;
+
+    private CancellationTokenSource selectedToken = new CancellationTokenSource();
+    private CancellationTokenSource deselectedToken = new CancellationTokenSource();
 
     public Shape GetShape()
     {
@@ -30,7 +45,7 @@ public class Point : MonoBehaviour
     public void Setup(int x1, int y1)
     {
         var setupInt = Random.Range(0, 5);
-        pointShape = (Shape) setupInt;
+        pointShape = (Shape)setupInt;
         image.sprite = iconList[setupInt];
         x = x1;
         y = y1;
@@ -39,5 +54,72 @@ public class Point : MonoBehaviour
     public bool IsEqual(Point point)
     {
         return point.x == x && point.y == y;
+    }
+
+    private void Select()
+    {
+        if (isSelected) return;
+        Selected?.Invoke();
+        isSelected = true;
+        ShowSelection();
+    }
+
+    public async void Deselect()
+    {
+        await Task.Delay(TimeSpan.FromSeconds(0.5f));
+        selectedToken?.Cancel();
+        deselectedToken = new CancellationTokenSource();
+        float lerp = 0;
+        var oldScale = gameObject.transform.localScale;
+        while (lerp < 1)
+        {
+            if (deselectedToken.IsCancellationRequested)
+            {
+                return;
+            }
+            gameObject.transform.localScale = Vector3.Lerp(oldScale, Vector3.one, lerp);
+            lerp += Time.deltaTime * 2;
+            await Task.Yield();
+        }
+        isScaled = false;
+        isSelected = false;
+        selectedToken?.Cancel();
+        deselectedToken?.Cancel();
+    }
+
+    private async void ShowSelection()
+    {
+        float lerp = 0;
+        var vectorTwo = new Vector3(1.5f, 1.5f, 1.5f);
+        selectedToken = new CancellationTokenSource();
+        while (lerp < 1)
+        {
+            if (selectedToken.IsCancellationRequested) return;
+            if (!isScaled)
+            {
+                gameObject.transform.localScale = Vector3.Lerp(Vector3.one, vectorTwo, lerp);
+            }
+            else
+            {
+                gameObject.transform.localScale = Vector3.Lerp(vectorTwo, Vector3.one,lerp);
+            }
+            lerp += Time.deltaTime;
+            await Task.Yield();
+        }
+        isScaled = !isScaled;
+        if (isSelected) ShowSelection();
+    }
+
+    private void Start()
+    {
+        button.onClick.AddListener(Select);
+    }
+
+    private void OnDestroy()
+    {
+        selectedToken?.Cancel();
+        selectedToken?.Dispose();
+        deselectedToken?.Cancel();
+        deselectedToken?.Dispose();
     }
 }
