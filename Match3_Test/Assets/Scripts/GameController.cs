15e44384fd7187cd02b7d772d6cadfe523ce9af1
Chell963 <buterbrod963@gmail.com>
2022-07-18 14:12:08 +1000

    Board setup & point spawn

--- Match3_Test/Assets/Scripts/GameController.cs
+++ Match3_Test/Assets/Scripts/GameController.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private int length = 8;
+    
+    [SerializeField] private Point basicPoint;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private GameObject pointParent;
+
+    [SerializeField] private Canvas mainCanvas;
+
+    private void Start()
     {
-        
+        for (var i = 0; i < length; i++)
+        {
+            for (var j = 0; j < length; j++)
+            {
+                var newPoint = Instantiate(basicPoint, pointParent.transform);
+                newPoint.Setup(j,length-1 - i);
+                var pixelRect = mainCanvas.pixelRect;
+                newPoint.transform.position = 
+                    new Vector2(-350 + j*100 + pixelRect.width/2,350 - i*100 + pixelRect.height/2);
+            }
+        }
     }
 }
